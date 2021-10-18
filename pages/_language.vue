f76b7747076022acb7c6a58d7da1f73a00e15246
Thijs Wijnmaalen <thijs@wijnmaalen.name>
2021-10-18 11:01:01 +0200

    Removed second editor; one editor with separate prettier config

--- pages/_language.vue
+++ pages/_language.vue
@@ -1,15 +1,40 @@
 <template>
-  <panes v-model="input" :output="output" :language="language" :error="error" />
+  <div class="flex">
+    <client-only>
+      <prism-editor
+        v-model="input"
+        :language="language"
+        line-numbers
+        :highlight="highlight"
+        class="input-editor w-4/5"
+        @input="(code) => $emit('input', code)"
+      />
+    </client-only>
+    <div class="w-1/5 bg-gray-700 p-4">
+      <textarea v-model="configuration" placeholder="JSON configuration" :class="{ 'border-transparent': config, 'border-red-400': !config }" class="p-2 bg-gray-900 text-sm text-white focus:bg-gray-900 rounded font-mono w-full h-48 focus:outline-none border-2 transition-colors duration-200"></textarea>
+      <button class="mt-2 bg-blue-600 text-white font-semibold px-3 py-1 rounded hover:bg-blue-500 transition-colors duration-150" @click.prevent="parse">Format</button>
+    </div>
+  </div>
 </template>
 
 <script>
+import { PrismEditor } from 'vue-prism-editor'
 import 'vue-prism-editor/dist/prismeditor.min.css'
+
+import { highlight as prism, languages } from 'prismjs/components/prism-core'
 import { format } from 'prettier/standalone'
-import Panes from '~/components/Panes'
+
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-clike'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-php'
+import 'prismjs/components/prism-sql'
+
+import 'prismjs/themes/prism-tomorrow.css'
 
 export default {
   components: {
-    Panes
+    PrismEditor
   },
 
   validate({ params }) {
@@ -18,92 +43,31 @@ export default {
     return language === 'sql' || language === 'php' || !language
   },
 
-  fetch() {
-    const key = `input.${this.language}`
-    const input = localStorage.getItem(key)
-    if (input) {
-      this.$nextTick(() => (this.input = input))
-    }
-  },
-
-  fetchOnServer: false,
-
   data() {
     return {
       input: '',
-      output: '',
       error: null,
-      plugin: null
+      plugin: null,
+      configuration: ''
     }
   },
 
-  computed: {
-    language() {
-      const { language } = this.$route.params
+  fetch() {
+    const input = localStorage.getItem(`input.${this.language}`)
+    const configuration = localStorage.getItem(`configuration.${this.language}`)
 
-      switch (language) {
-        case 'php':
-        case 'sql':
-          return language
-        default:
-          return 'javascript'
-      }
-    }
+    this.$nextTick(() => {
+      this.input = input || ''
+      this.configuration = configuration || '{\n' +
+        '  "trailingComma": "es5",\n' +
+        '  "tabWidth": 2,\n' +
+        '  "semi": false,\n' +
+        '  "singleQuote": true\n' +
+        '}'
+    })
   },
 
-  watch: {
-    language: {
-      async handler() {
-        const lang = this.language
-
-        if (lang === 'javascript') {
-          this.plugin = await import('prettier/parser-babel')
-        }
-
-        if (lang === 'php') {
-          this.plugin = await import('@prettier/plugin-php/standalone')
-        }
-
-        if (lang === 'sql') {
-          this.plugin = await import('sql-formatter')
-        }
-
-        this.parse()
-      },
-      immediate: true
-    },
-
-    input() {
-      const key = `input.${this.language}`
-      localStorage.setItem(key, this.input)
-
-      this.parse()
-    }
-  },
-
-  methods: {
-    parse() {
-      if (!this.plugin) {
-        return
-      }
-
-      try {
-        if (this.language === 'sql') {
-          this.output = this.plugin.format(this.input)
-          return
-        }
-
-        this.output = format(this.input, {
-          parser: this.language === 'javascript' ? 'babel' : this.language,
-          plugins: [this.plugin]
-        })
-
-        this.error = null
-      } catch (error) {
-        this.error = error
-      }
-    }
-  },
+  fetchOnServer: false,
 
   head() {
     let language = ''
@@ -128,6 +92,98 @@ export default {
         }
       ]
     }
+  },
+
+  computed: {
+    language() {
+      const { language } = this.$route.params
+
+      switch (language) {
+        case 'php':
+        case 'sql':
+          return language
+        default:
+          return 'javascript'
+      }
+    },
+
+    config() {
+      try {
+        return JSON.parse(this.configuration)
+      } catch {
+        return null
+      }
+    }
+  },
+
+  watch: {
+    language: {
+      async handler() {
+        const lang = this.language
+
+        if (lang === 'javascript') {
+          this.plugin = await import('prettier/parser-babel')
+        }
+
+        if (lang === 'php') {
+          this.plugin = await import('@prettier/plugin-php/standalone')
+        }
+
+        if (lang === 'sql') {
+          this.plugin = await import('sql-formatter')
+        }
+      },
+      immediate: true
+    },
+
+    input() {
+      localStorage.setItem(`input.${this.language}`, this.input)
+    },
+
+    configuration() {
+      localStorage.setItem(`configuration.${this.language}`, this.configuration)
+    }
+  },
+
+  methods: {
+    highlight(code) {
+      return prism(code, languages[this.language])
+    },
+
+    parse() {
+      if (!this.plugin) {
+        return
+      }
+
+      try {
+        if (this.language === 'sql') {
+          this.input = this.plugin.format(this.input)
+          return
+        }
+
+        this.input = format(this.input, {
+          ...(this.config || {}),
+          parser: this.language === 'javascript' ? 'babel' : this.language,
+          plugins: [this.plugin]
+        })
+
+        this.error = null
+      } catch (error) {
+        this.error = error
+      }
+    }
   }
 }
 </script>
+
+<style>
+/* purgecss start ignore */
+.input-editor {
+  @apply font-mono text-sm leading-6 p-2 text-gray-100 bg-gray-900;
+}
+
+.prism-editor__textarea:focus {
+  @apply outline-none;
+}
+/* purgecss end ignore */
+</style>
